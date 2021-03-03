5a03ea2539acbf614a9444efb0f1d20193d69732
ethan.mcneal <ethan.mcneal@gmail.com>
2021-03-03 16:27:58 -0700

    initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,7 @@
+class BooksController < ApplicationController
+
+    def app
+        render component: 'App'
+    end
+
+end
---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import Books from './Books'
+
+const App = () => {
+    const [books, setBooks] = useState([])
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
